fbae5f654b5791f77b18eadc7041f541fc07412e
ako-tech <akotech20@gmail.com>
2023-01-31 18:09:37 +0100

    refactor for video

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,8 @@
-import { fromEvent, race, repeat, switchMap, take, timer } from "rxjs";
+import { fromEvent, switchMap } from "rxjs";
 import { updateResults } from "./results-manager";
 import "./style.css";
 
 const refreshButton = document.getElementById("refresh-button");
 const refreshButtonClick$ = fromEvent(refreshButton!, "click");
 
-race([refreshButtonClick$.pipe(take(1)), timer(5_000)])
-    .pipe(
-        switchMap((_) => updateResults()),
-        repeat()
-    )
-    .subscribe();
+refreshButtonClick$.pipe(switchMap((_) => updateResults())).subscribe();
