dc94f8ac6ac9340839af6727fda5b909297d83f3
ako-tech <akotech20@gmail.com>
2023-01-31 18:12:01 +0100

    change to exhaustmap

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { fromEvent, switchMap } from "rxjs";
+import { fromEvent, exhaustMap } from "rxjs";
 import { updateResults } from "./results-manager";
 import "./style.css";
 
 const refreshButton = document.getElementById("refresh-button");
 const refreshButtonClick$ = fromEvent(refreshButton!, "click");
 
-refreshButtonClick$.pipe(switchMap((_) => updateResults())).subscribe();
+refreshButtonClick$.pipe(exhaustMap((_) => updateResults())).subscribe();
