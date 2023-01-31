209cad2d2220757fcdda2b5c8ba039ebd45cf84d
ako-tech <akotech20@gmail.com>
2023-01-31 17:51:57 +0100

    simplify example

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,13 @@
-import {
-    fromEvent,
-    map,
-    tap,
-    race,
-    timer,
-    repeat,
-    switchMap,
-    take,
-    last,
-} from "rxjs";
-import { updateTimeLeftLabel } from "./renderer";
-import { loader, updateResults } from "./results-manager";
+import { fromEvent, race, repeat, switchMap, timer } from "rxjs";
+import { updateResults } from "./results-manager";
 import "./style.css";
 
 const refreshButton = document.getElementById("refresh-button");
 const refreshButtonClick$ = fromEvent(refreshButton!, "click");
-const manualRefresh$ = refreshButtonClick$.pipe(take(1));
 
-const autoRefreshTimeInSeconds = 5;
-const autoRefresh$ = timer(0, 1_000).pipe(
-    take(autoRefreshTimeInSeconds + 1),
-    map((secondsElapsed) => autoRefreshTimeInSeconds - secondsElapsed),
-    tap((secondsLeft) => updateTimeLeftLabel(secondsLeft)),
-    last()
-);
-
-race([manualRefresh$, autoRefresh$])
+race([refreshButtonClick$, timer(5_000)])
     .pipe(
-        switchMap((_) => race([loader(), updateResults()])),
-        repeat(2)
+        switchMap((_) => updateResults()),
+        repeat()
     )
     .subscribe();
